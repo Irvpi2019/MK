87fdefbbf168ab3eb88a8a1ec6d05302b6f1c8e1
Irina Pinchuk <irvpi2015@hotmail.com>
2021-10-14 20:52:33 +0200

    homework-2

---
+++ main.js
@@ -0,0 +1,30 @@
+//Task #0
+const player1 = {
+    name: 'Sonya',
+    hp: 100,
+    img: 'img1',
+    weapon: ['W1', 'W2' ,'W3'],
+    attack: function getMessage(name) {
+        console.log(name + '' + 'Fight...');
+    }
+};
+
+const player1 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: 'img2',
+    weapon: ['W1', 'W2' ,'W3'],
+    attack: function getMessage(name) {
+        console.log(name + '' + 'Fight...');
+    }
+};
+
+//Task #1
+function createPlayer() {
+    const plaier1$ = document.createElement('div');
+    const progressbar = document.createElement('div');
+    const character = document.createElement('div');
+    
+}
+
+//Task #2
