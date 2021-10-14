94164528555f02d271fabfaa340899f04fc63459
Irina Pinchuk <irvpi2015@hotmail.com>
2021-10-14 23:10:15 +0200

    Changed main.js

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 const player1 = {
     name: 'Sonya',
     hp: 100,
-    img: 'img1',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['W1', 'W2' ,'W3'],
     attack: function getMessage(name) {
         console.log(name + '' + 'Fight...');
     }
 };
 
-const player1 = {
+const player2 = {
     name: 'Scorpion',
     hp: 100,
-    img: 'img2',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['W1', 'W2' ,'W3'],
     attack: function getMessage(name) {
         console.log(name + '' + 'Fight...');
@@ -21,10 +21,17 @@ const player1 = {
 
 //Task #1
 function createPlayer() {
-    const plaier1$ = document.createElement('div');
-    const progressbar = document.createElement('div');
-    const character = document.createElement('div');
+    const $player1 = document.createElement('div');
+    const $progressbar = document.createElement('div');
+    const $character = document.createElement('div');
+    const $life = document.createElement('div');
+    const $name = document.createElement('div');
+    const $img = document.createElement('div');
+
+    $player1.classList.add('player1');
+
     
 }
 
+//createPlayer();
 //Task #2
